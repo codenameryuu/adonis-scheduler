852f52e8ad455b978b025f5c8249f6d0e3b7bee6
codenameryuu <leaderballon@gmail.com>
2026-06-28 15:49:09 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,186 @@
+<div align="center">
+  <h1><b>AdonisJS Scheduler</b></h1>
+
+  <p>Task scheduler for AdonisJS v6</p>
+
+  <p>
+    <a href="https://npmjs.org/package/adonisjs-scheduler" target="_blank">
+      <img alt="npm" src="https://img.shields.io/npm/v/adonisjs-scheduler.svg?style=for-the-badge&logo=npm" />
+    </a>
+    <a href="https://github.com/KABBOUCHI/adonisjs-scheduler/blob/master/LICENSE.md" target="_blank">
+      <img alt="License: MIT" src="https://img.shields.io/npm/l/adonisjs-scheduler?color=blueviolet&style=for-the-badge" />
+    </a>
+    <img alt="Typescript" src="https://img.shields.io/badge/Typescript-294E80.svg?style=for-the-badge&logo=typescript" />
+  </p>
+</div>
+
+<p align="center">
+    <img src="https://raw.githubusercontent.com/KABBOUCHI/adonisjs-scheduler/master/example.png" alt="Scheduler code example">
+</p>
+
+<aside class="notice">
+  <a href="https://github.com/KABBOUCHI/adonisjs-scheduler/tree/0.x">For AdonisJS v5 use 0.x branch</a>
+</aside>
+
+## Installation
+
+Install the package from npm using the following command:
+
+```bash
+node ace add adonisjs-scheduler
+```
+
+## Running The Scheduler
+
+```sh
+node ace scheduler:run
+# or
+node ace scheduler:work
+
+# automatically restart the scheduler when files are modified during development mode
+node ace scheduler:run --watch
+
+
+node ace scheduler:run --tag <tag_name> # Run all schedules with the specified tag
+```
+
+## Defining Schedules
+
+```ts
+// start/scheduler.ts
+
+import scheduler from 'adonisjs-scheduler/services/main'
+
+import PurgeUsers from '../commands/purge_users'
+
+scheduler.command('inspire').everyFiveSeconds()
+scheduler.command(PurgeUsers, ['30 days']).everyFiveSeconds().withoutOverlapping()
+
+scheduler.withoutOverlapping(
+  () => {
+    scheduler.command('inspire').everySecond()
+    scheduler.command(PurgeUsers, ['30 days']).everyFiveSeconds()
+  },
+  { expiresAt: 30_000 }
+)
+
+scheduler.withTag(
+  () => {
+    scheduler.command('inspire').everySecond()
+  },
+  'my-tag'
+)
+
+scheduler
+  .call(() => {
+    console.log('Pruge DB!')
+  })
+  .weekly()
+
+
+scheduler
+  .call(() => {
+    console.log('Pruge DB!')
+  })
+  .tag('my-tag')
+  .weekly()
+```
+
+or define schedule directly on command
+
+```ts
+import { BaseCommand, args } from '@adonisjs/core/ace'
+import { schedule } from 'adonisjs-scheduler'
+
+@schedule("* * * * *", ["30 days"])
+@schedule((s) => s.everyFiveSeconds().immediate(), ["7 days"])
+@schedule((s) => s.everyMinute(), ["42 days"])
+export default class PurgeUsers extends BaseCommand {
+  static commandName = 'purge:users'
+  static description = ''
+
+  static options: CommandOptions = {}
+
+  @args.string()
+  declare olderThan: string
+
+  async run() {
+    //
+  }
+}
+```
+
+
+## Schedule Frequency Options
+
+| Method                           | Description                                             |
+| -------------------------------- | ------------------------------------------------------- |
+| `.cron('* * * * *');`            | Run the task on a custom cron schedule                  |
+| `.everyMinute();`                | Run the task every minute                               |
+| `.everyTwoMinutes();`            | Run the task every two minutes                          |
+| `.everyThreeMinutes();`          | Run the task every three minutes                        |
+| `.everyFourMinutes();`           | Run the task every four minutes                         |
+| `.everyFiveMinutes();`           | Run the task every five minutes                         |
+| `.everyTenMinutes();`            | Run the task every ten minutes                          |
+| `.everyFifteenMinutes();`        | Run the task every fifteen minutes                      |
+| `.everyThirtyMinutes();`         | Run the task every thirty minutes                       |
+| `.hourly();`                     | Run the task every hour                                 |
+| `.hourlyAt(17);`                 | Run the task every hour at 17 minutes past the hour.    |
+| `.everyTwoHours();`              | Run the task every two hours                            |
+| `.everyThreeHours();`            | Run the task every three hours                          |
+| `.everyFourHours();`             | Run the task every four hours                           |
+| `.everyFiveHours();`             | Run the task every five hours                           |
+| `.everySixHours();`              | Run the task every six hours                            |
+| `.daily();`                      | Run the task every day at midnight                      |
+| `.dailyAt('13:00');`             | Run the task every day at 13:00.                        |
+| `.twiceDaily(1, 13);`            | Run the task daily at 1:00 & 13:00.                     |
+| `.twiceDailyAt(1, 13, 15);`      | Run the task daily at 1:15 & 13:15.                     |
+| `.weekly();`                     | Run the task every Sunday at 00:00                      |
+| `.weeklyOn(1, '8:00');`          | Run the task every week on Monday at 8:00.              |
+| `.monthly();`                    | Run the task on the first day of every month at 00:00   |
+| `.monthlyOn(4, '15:00');`        | Run the task every month on the 4th at 15:00.           |
+| `.twiceMonthly(1, 16, '13:00');` | Run the task monthly on the 1st and 16th at 13:00.      |
+| `.lastDayOfMonth('15:00');`      | Run the task on the last day of the month at 15:00.     |
+| `.quarterly();`                  | Run the task on the first day of every quarter at 00:00.|
+| `.quarterlyOn(4, '14:00');`      | Run the task every quarter on the 4th at 14:00.         |
+| `.yearly();`                     | Run the task on the first day of every year at 00:00.   |
+| `.yearlyOn(6, 1, '17:00');`      | Run the task every year on June 1st at 17:00.           |
+| `.timezone('America/New_York');` | Set the timezone for the task.                          |
+| `.immediate();`                  | Run the task on startup                                 |
+| `.withoutOverlapping();`         | Run the task without overlapping                        |
+
+
+## Alternative Ways to Run the Scheduler
+
+Besides using `node ace scheduler:run`, you can also manually initialize and control the scheduler worker in your code:
+
+```ts
+import { Worker } from 'adonisjs-scheduler'
+import app from '@adonisjs/core/services/app'
+
+const worker = new Worker(app)
+
+app.terminating(async () => {
+  await worker.stop()
+})
+
+await worker.start()
+```
+
+
+Note: make sure to modify `adonisrc.ts` and include the environment where the worker is running.
+
+```ts
+import { defineConfig } from '@adonisjs/core/app'
+
+export default defineConfig({
+ providers: [
+   {
+      file: () => import('adonisjs-scheduler/scheduler_provider'),
+      environment: ['console', 'web'], // <---
+   },
+
+   // or enable for all environments
+   () => import('adonisjs-scheduler/scheduler_provider'),
+})
+```
---
+++ src/scheduler.ts
@@ -0,0 +1,446 @@
+import { BaseCommand, FsLoader } from '@adonisjs/core/ace'
+import type { ApplicationService } from '@adonisjs/core/types'
+import { DateTime } from 'luxon'
+import { arrayWrap } from './utils.js'
+
+type Range<
+  START extends number,
+  END extends number,
+  ARR extends unknown[] = [],
+  ACC extends number = never
+> = ARR['length'] extends END
+  ? ACC | START | END
+  : Range<START, END, [...ARR, 1], ARR[START] extends undefined ? ACC : ACC | ARR['length']>
+type DAYS = 0 | 1 | 2 | 3 | 4 | 5 | 6 | '0' | '1' | '2' | '3' | '4' | '5' | '6'
+
+export abstract class BaseSchedule {
+  SUNDAY = 0 as const
+  MONDAY = 1 as const
+  TUESDAY = 2 as const
+  WEDNESDAY = 3 as const
+  THURSDAY = 4 as const
+  FRIDAY = 5 as const
+  SATURDAY = 6 as const
+
+  abstract type: string
+  expression: string = '0 * * * * *' // seconds minutes hours dayOfMonth month dayOfWeek
+  config = {
+    tag: 'default',
+    enabled: true,
+    immediate: false,
+    withoutOverlapping: false,
+    expiresAt: 3600000,
+    timezone: undefined as string | undefined,
+  }
+
+  beforeCallbacks: (() => Promise<void>)[] = []
+  afterCallbacks: (() => Promise<void>)[] = []
+
+  public before(callback: () => Promise<void>) {
+    this.beforeCallbacks.push(callback)
+
+    return this
+  }
+
+  public after(callback: () => Promise<void>) {
+    this.afterCallbacks.push(callback)
+
+    return this
+  }
+
+  public timezone(timezone: string) {
+    this.config.timezone = timezone
+
+    return this
+  }
+
+  public skip(state: boolean = true) {
+    this.config.enabled = !state
+
+    return this
+  }
+
+  public tag(tag: string) {
+    this.config.tag = tag
+
+    return this
+  }
+
+  public immediate(state: boolean = true) {
+    this.config.immediate = state
+
+    return this
+  }
+
+  public withoutOverlapping(expiresAt: number = 3600000) {
+    this.config.withoutOverlapping = true
+    this.config.expiresAt = expiresAt
+
+    return this
+  }
+
+  public everyMinutes(minutes: number) {
+    return this.spliceIntoPosition(1, `*/${minutes}`)
+  }
+
+  public everyMinute() {
+    return this.spliceIntoPosition(1, '*')
+  }
+
+  public everyTwoMinutes() {
+    return this.everyMinutes(2)
+  }
+
+  public everyThreeMinutes() {
+    return this.everyMinutes(3)
+  }
+
+  public everyFourMinutes() {
+    return this.everyMinutes(4)
+  }
+
+  public everyFiveMinutes() {
+    return this.everyMinutes(5)
+  }
+
+  public everyTenMinutes() {
+    return this.everyMinutes(10)
+  }
+
+  public everyFifteenMinutes() {
+    return this.everyMinutes(15)
+  }
+
+  public everyThirtyMinutes() {
+    return this.everyMinutes(30)
+  }
+
+  public hourly() {
+    return this.spliceIntoPosition(1, 0)
+  }
+
+  public hourlyAt(offset: string | number | Range<0, 59> | Range<0, 59>[]) {
+    return this.hourBasedSchedule(offset, '*')
+  }
+
+  public everyHours(hours: number, offset: any[] | string | number = 0) {
+    return this.hourBasedSchedule(offset, `*/${hours}`)
+  }
+  public everyOddHour(offset: any[] | string | number = 0) {
+    return this.hourBasedSchedule(offset, '1-23/2')
+  }
+
+  public everyTwoHours(offset: any[] | string | number = 0) {
+    return this.everyHours(2, offset)
+  }
+
+  public everyThreeHours(offset: any[] | string | number = 0) {
+    return this.everyHours(3, offset)
+  }
+
+  public everyFourHours(offset: any[] | string | number = 0) {
+    return this.everyHours(4, offset)
+  }
+
+  public everyFiveHours(offset: any[] | string | number = 0) {
+    return this.everyHours(5, offset)
+  }
+
+  public everySixHours(offset: any[] | string | number = 0) {
+    return this.everyHours(6, offset)
+  }
+
+  public daily() {
+    return this.hourBasedSchedule(0, 0)
+  }
+
+  public weekdays() {
+    return this.spliceIntoPosition(5, '1-5')
+  }
+
+  public weekends() {
+    return this.spliceIntoPosition(5, '6,0')
+  }
+
+  public mondays() {
+    return this.days(1)
+  }
+
+  public tuesdays() {
+    return this.days(2)
+  }
+
+  public wednesdays() {
+    return this.days(3)
+  }
+
+  public thursdays() {
+    return this.days(4)
+  }
+
+  public fridays() {
+    return this.days(5)
+  }
+
+  public saturdays() {
+    return this.days(6)
+  }
+
+  public sundays() {
+    return this.days(0)
+  }
+
+  public weekly() {
+    return this.spliceIntoPosition(1, 0).spliceIntoPosition(2, 0).spliceIntoPosition(5, 0)
+  }
+
+  public weeklyOn(dayOfWeek: Range<0, 7> = 1, time: string = '0:0') {
+    this.dailyAt(time)
+
+    return this.days(dayOfWeek as any)
+  }
+
+  public monthly() {
+    return this.spliceIntoPosition(1, 0).spliceIntoPosition(2, 0).spliceIntoPosition(3, 1)
+  }
+
+  public quarterly() {
+    return this.spliceIntoPosition(1, 0)
+      .spliceIntoPosition(2, 0)
+      .spliceIntoPosition(3, 1)
+      .spliceIntoPosition(4, '1-12/3')
+  }
+
+  public yearly() {
+    return this.spliceIntoPosition(1, 0)
+      .spliceIntoPosition(2, 0)
+      .spliceIntoPosition(3, 1)
+      .spliceIntoPosition(4, 1)
+  }
+
+  public yearlyOn(month: number = 1, dayOfMonth: string | Range<1, 31> = 1, time: string = '0:0') {
+    this.dailyAt(time)
+
+    return this.spliceIntoPosition(3, dayOfMonth).spliceIntoPosition(4, month)
+  }
+
+  public everySecond() {
+    return this.spliceIntoPosition(0, '*')
+  }
+
+  public everySeconds(second: Range<1, 59>) {
+    return this.spliceIntoPosition(0, `*/${second}`)
+  }
+
+  public everyFiveSeconds() {
+    return this.everySeconds(5)
+  }
+
+  public everyTenSeconds() {
+    return this.everySeconds(10)
+  }
+
+  public everyFifteenSeconds() {
+    return this.everySeconds(15)
+  }
+
+  public everyThirtySeconds() {
+    return this.everySeconds(30)
+  }
+
+  public cron(expression: string) {
+    this.expression = expression
+
+    return this
+  }
+
+  protected spliceIntoPosition(position: number, value: string | number) {
+    const segements = this.expression.split(' ')
+
+    segements[position] = String(value)
+
+    this.cron(segements.join(' '))
+
+    return this
+  }
+
+  protected hourBasedSchedule(
+    minutes: string | number | Range<0, 59>[],
+    hours: string | number | Range<0, 59>[]
+  ) {
+    minutes = Array.isArray(minutes) ? minutes.join(',') : minutes
+    hours = Array.isArray(hours) ? hours.join(',') : hours
+
+    return this.spliceIntoPosition(1, minutes).spliceIntoPosition(2, hours)
+  }
+
+  public days(days: DAYS | DAYS[]) {
+    return this.spliceIntoPosition(5, Array.isArray(days) ? days.join(',') : days)
+  }
+
+  public at(time: string) {
+    return this.dailyAt(time)
+  }
+
+  public dailyAt(time: string) {
+    let segments = time.split(':')
+
+    return this.hourBasedSchedule(
+      segments.length === 2 ? Number(segments[1]) : '0',
+      Number(segments[0])
+    )
+  }
+
+  public twiceDailyAt(first: Range<0, 23> = 1, second: Range<0, 23> = 13, offset = 0) {
+    const hours = first + ',' + second
+
+    return this.hourBasedSchedule(offset, hours)
+  }
+
+  public twiceDaily(first: Range<0, 23> = 1, second: Range<0, 23> = 13) {
+    return this.twiceDailyAt(first, second, 0)
+  }
+
+  public twiceMonthly(first: Range<1, 31> = 1, second: Range<1, 31> = 13, time: string = '0:0') {
+    const dayOfMonth = first + ',' + second
+
+    this.dailyAt(time)
+
+    return this.spliceIntoPosition(3, dayOfMonth)
+  }
+
+  public lastDayOfMonth(time: string = '0:0') {
+    this.dailyAt(time)
+
+    return this.spliceIntoPosition(3, DateTime.now().endOf('month').day)
+  }
+
+  public monthlyOn(dayOfMonth: Range<1, 31> = 1, time: string = '0:0') {
+    this.dailyAt(time)
+
+    return this.spliceIntoPosition(3, dayOfMonth)
+  }
+
+  public getExpression() {
+    return this.expression
+  }
+}
+
+export class ScheduleCommand extends BaseSchedule {
+  type: 'command' = 'command'
+
+  commandName: string
+  commandArgs: string[]
+
+  constructor(commandName: string, commandArgs: string[] = []) {
+    super()
+
+    this.commandName = commandName
+    this.commandArgs = commandArgs
+  }
+}
+
+export class ScheduleCallback extends BaseSchedule {
+  type: 'callback' = 'callback'
+
+  callback: Function
+
+  constructor(callback: Function) {
+    super()
+
+    this.callback = callback
+  }
+}
+
+export class Scheduler {
+  SUNDAY = 0 as const
+  MONDAY = 1 as const
+  TUESDAY = 2 as const
+  WEDNESDAY = 3 as const
+  THURSDAY = 4 as const
+  FRIDAY = 5 as const
+  SATURDAY = 6 as const
+
+  constructor(protected app: ApplicationService) {
+    this.app = app
+  }
+
+  static __decorator_schedules: (ScheduleCallback | ScheduleCommand)[] = []
+
+  items: (ScheduleCallback | ScheduleCommand)[] = []
+
+  onStartingCallback?: ({ tag }: { tag: string }) => void | Promise<void>
+  onStartedCallback?: ({ tag }: { tag: string }) => void | Promise<void>
+  onBootCallback?: () => void | Promise<void>
+
+  public async boot() {
+    await this.onBootCallback?.()
+
+    const fsLoader = new FsLoader<typeof BaseCommand>(this.app.commandsPath())
+    await fsLoader.getMetaData()
+
+    for (const command of this.app.rcFile.commands) {
+      const loader = await (typeof command === 'function' ? command() : command)
+      await loader.getMetaData()
+    }
+
+    if (!Scheduler.__decorator_schedules || Scheduler.__decorator_schedules.length === 0) return
+
+    this.items.push(...Scheduler.__decorator_schedules)
+  }
+
+  public command(name: string | typeof BaseCommand, args: string | string[] = []) {
+    let newCommand = new ScheduleCommand(
+      typeof name === 'string' ? name : name.commandName,
+      arrayWrap(args)
+    )
+
+    this.items.push(newCommand)
+
+    return newCommand
+  }
+
+  public call(callback: Function) {
+    let newCommand = new ScheduleCallback(callback)
+
+    this.items.push(newCommand)
+
+    return newCommand
+  }
+
+  public withoutOverlapping(callback: () => void, config = { expiresAt: 3600000 }) {
+    const lastLength = this.items.length
+    callback()
+    const currentLength = this.items.length
+
+    const newItems = this.items.slice(lastLength, currentLength)
+
+    for (const item of newItems) {
+      item.withoutOverlapping(config.expiresAt)
+    }
+  }
+
+  public withTag(callback: () => void, tag: string) {
+    const lastLength = this.items.length
+    callback()
+    const currentLength = this.items.length
+
+    const newItems = this.items.slice(lastLength, currentLength)
+
+    for (const item of newItems) {
+      item.tag(tag)
+    }
+  }
+
+  public onStarting(callback: ({ tag }: { tag: string }) => void | Promise<void>) {
+    this.onStartingCallback = callback
+  }
+
+  public onStarted(callback: ({ tag }: { tag: string }) => void | Promise<void>) {
+    this.onStartedCallback = callback
+  }
+
+  public onBoot(callback: () => void | Promise<void>) {
+    this.onBootCallback = callback
+  }
+}
